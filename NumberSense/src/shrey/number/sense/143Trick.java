bb5fb1039e4306f14a747f1afe9e5931499ea127
Vishnu Iyer <vishnu72498@gmail.com>
2014-06-10 17:04:26 -0500

    Update and rename 143.java to 143Trick.java

---
+++ NumberSense/src/shrey/number/sense/143Trick.java
@@ -0,0 +1,16 @@
+// Trick to multiply number by 143
+// the trick is to divide the number by 7 and multipy that by 1001
+// updated by Vishnu Iyer
+
+public class 143Trick
+{
+  public String toL [] ()
+  {
+    int val1 = 7 * ((int)(Math.random()*15)+5);
+    int val2 = 143 * val1;
+    String [] st = new String [3];
+    st[0] = String.format("\\item \\( %d \\times %d \\)", val1, 143);
+    st[1] = st[1] = String.format("\\item \\( %d \\)", val2);
+    return st;
+  }
+}
