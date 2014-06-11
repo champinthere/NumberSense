685e704f0695fe17fdcf551233b5995b82ee6d1e
Vishnu Iyer <vishnu72498@gmail.com>
2014-06-10 22:49:20 -0500

    Update SummingSquares.java

--- NumberSense/src/shrey/number/sense/SummingSquares.java
+++ NumberSense/src/shrey/number/sense/SummingSquares.java
@@ -1,4 +1,34 @@
 public SummingSquares extends Trick
 {
-  
+  public String [] tL()
+  {
+    String [] st = new String [3];
+    st[2] = "";
+    int a = 0;
+    int b = 0;
+    int c = (int)(Math.random()*3);
+    switch(c)
+    {
+      case 0:
+        a = (int)(Math.random()*18)+13;
+        b = 3*a;
+        break;
+      case 1:
+        a = (int)(Math.random()*11)+10;
+        b = 7*a;
+        break;
+      case 3:
+        boolean c = true;
+        while(c)
+        {
+          a = (int)(Math.random()*71)+15;
+          c = a%10==0;
+        }
+        b = 10*(a%10-1)+(10-a/10);
+        break;
+    }
+    int d = Math.pow(a,2)+Math.pow(b,2);
+    st[0] = String.format("\\item \\( %d \\times %d \\plus %d \\times %d \\)", a, a, b, b);
+    st[1] = String.format("\\item \\( %d \\)", d); 
+  }
 }
